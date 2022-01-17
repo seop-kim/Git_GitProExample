535f2b5e0e6ebb32021350a624c0f3563296dda7
tseop <77163842+tseop@users.noreply.github.com>
2022-01-17 17:16:42 +0900

    frist commit

---
+++ src/com/gitpro/example/GitExample1.java
@@ -0,0 +1,9 @@
+package com.gitpro.example;
+
+public class GitExample1 {
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		
+		System.out.println("Git Status 실험");
+	}
+}
